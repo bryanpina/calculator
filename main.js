8623ce4fc573ece90bdd503823fcda7047483e9e
Bryan <109005501+bryanpina@users.noreply.github.com>
2023-11-14 16:25:12 +0900

    ADDED STYLING TO CALCULATOR, ADDED FUNCTION Centered buttons to center using flex, added incomplete function that populates numbers pressed in dispaly to js

---
+++ main.js
@@ -0,0 +1,29 @@
+// Variables for calculator operation (number, operator, another number) will be used to update my display later
+
+let num1 = "";
+let operator = "";
+let num2 = "";
+
+// Functions for add, subtract, multiply, and divide
+
+function add(){
+
+}
+
+function subtract(){
+
+}
+
+function multiply(){
+
+}
+
+function divide(){
+
+}
+
+//Function that takes an operator and 2 numbers and then calls one of the above functions on the numbers (add, subtract, multiply, divide)
+
+function operate(){
+    
+}
